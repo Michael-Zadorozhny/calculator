4230eef94a931be6546bbc998b45cae87f75ee46
MZ <mike.zadorozhny@gmail.com>
2023-08-10 19:41:43 -0400

    Calculator UI complete

---
+++ app.js
@@ -0,0 +1,18 @@
+let currentNumber = '';
+let previousNumber = '';
+let operator = '';
+
+const currentDisplayNumber = document.querySelector('.currentNumber');
+const previousDisplayNumber = document.querySelector('.previousNumber');
+const equal = document.querySelector('.equal');
+const decimal = document.querySelector('.decimal');
+const clear = document.querySelector('.clear');
+
+const numberButtons = document.querySelectorAll('.number')
+const operatorButtons = document.querySelectorAll('.operator')
+
+numberButtons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        handleNumber
+    })
+})
